a22b3000354844bc6937a6c9d7f90690079fc9a4
Idanturjeman <idan_turjeman29@walla.co.il>
2022-04-30 19:20:35 +0300

    change captain

--- Captain.cpp
+++ Captain.cpp
@@ -1,20 +1,22 @@
 #include "Captain.hpp"
 namespace coup
 {
+    static int numOfCaptains = 0;
     Captain::Captain(Game &game, string name) : Player(game,name)
     {
-    }
-
-    Captain::~Captain()
-    {
-    }
-    void Captain::block(Player p){
-        cout<<"block\n";
-    }
-    void Captain::steal(Player p){
-        cout << "stral\n";
+        numOfCaptains++;
     }
     void Captain::role(){
         cout<<"Captain"<<endl;
+    }
+    void Captain::steal(Player p){
+        cout << "stral"<<endl;
+    }
+    void Captain::block(Player p){
+        cout<<"block"<<endl;
+    } 
+    Captain::~Captain()
+    {
+        numOfCaptains--;
     }
 }
