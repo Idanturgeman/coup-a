9d8e1eccf58facac9caca1e7c62f5c9939054f6f
Idanturjeman <idan_turjeman29@walla.co.il>
2022-04-30 20:24:40 +0300

    change player

--- Player.cpp
+++ Player.cpp
@@ -4,45 +4,51 @@ using namespace std;
 
 namespace coup
 {
+    static int numOfPlayers = 0;
+    static int numOfIncomes = 0;
+    static int numOfForeign_aids = 0;
+    static int numOfCoups = 0;
+    static int numOfRoles = 0;
+    static int numOfCoins = 0;
     Player::Player(Game &game, string name)
     {
         //setPlayer(Game &game, string name);
         // game.addPlayer(this);
         //setName(name);
-        int numOfPlayer = 0;
+        numOfPlayers++;
         this->_name = name;
         this->_game = &game;
-        int numOfPlaye = 0;
+       // int numOfPlaye = 0;
         this->_game->playersList.push_back(this->_name);
-        int numOfPlay = 0;
+       // int numOfPlay = 0;
         this->_money = 0;
     }
     void Player::income()
     {
         this->_money++;
-        int numOfIncomes = 0;
+        numOfIncomes++;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
-        int numOfIncome = 1;
+       // int numOfIncome = 1;
 
     }
     void Player::foreign_aid()
     {
         this->_money += 2;
-        int numOfForeign_aids = 0;
+        numOfForeign_aids++;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
-        int numOfForeign_aid = 1;
+       // int numOfForeign_aid = 1;
 
     }
 
     void Player::coup(Player p)
     {
-        int numOfCoups = 0;
+        numOfCoups++;
         for (unsigned int i = 0; i < this->_game->playersList.size(); i++)
             {
-                int numOfCoup = 0;
+                //int numOfCoup = 0;
                 if (!p._name.compare(this->_game->playersList[i]))
                     {
-                        int numOfCou = 1;
+                        //int numOfCou = 1;
                         this->_game->playersList.erase(this->_game->playersList.begin() + i);
                         return;
                     }
@@ -54,17 +60,17 @@ namespace coup
 
     void Player::role()
     {
-        int numOfRoles = 0 ;
+        numOfRoles++;
         cout << "jobName";
     }
     int Player::coins()
     {
-        int numOfCoins = 0;
+        numOfCoins++;
         return getMoney();
     }
     
     Player::~Player()
     {
-        int numOfPlayers = 0;
+        numOfPlayers--;
     }
 }
