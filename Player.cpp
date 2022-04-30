921a6de1dcdeda98f070a28943b6c2df39cc1326
Idanturjeman <idan_turjeman29@walla.co.il>
2022-04-30 18:34:58 +0300

    change player

--- Player.cpp
+++ Player.cpp
@@ -4,28 +4,45 @@ using namespace std;
 
 namespace coup
 {
+    Player::Player(Game &game, string name)
+    {
+        //setPlayer(Game &game, string name);
+        // game.addPlayer(this);
+        //setName(name);
+        int numOfPlayer = 0;
+        this->_name = name;
+        this->_game = &game;
+        int numOfPlaye = 0;
+        this->_game->playersList.push_back(this->_name);
+        int numOfPlay = 0;
+        this->_money = 0;
+    }
     void Player::income()
     {
         this->_money++;
         int numOfIncomes = 0;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
+        int numOfIncome = 1;
+
     }
     void Player::foreign_aid()
     {
         this->_money += 2;
-        int numOfForeign_aid = 0;
+        int numOfForeign_aids = 0;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
+        int numOfForeign_aid = 1;
+
     }
 
     void Player::coup(Player p)
     {
-        
+        int numOfCoups = 0;
         for (unsigned int i = 0; i < this->_game->playersList.size(); i++)
             {
-                int numOfCoups = 0;
+                int numOfCoup = 0;
                 if (!p._name.compare(this->_game->playersList[i]))
                     {
-                        int numOfCoups = 0;
+                        int numOfCou = 1;
                         this->_game->playersList.erase(this->_game->playersList.begin() + i);
                         return;
                     }
@@ -45,16 +62,7 @@ namespace coup
         int numOfCoins = 0;
         return getMoney();
     }
-    Player::Player(Game &game, string name)
-    {
-        //setPlayer(Game &game, string name);
-        // game.addPlayer(this);
-        int numOfPlayers = 0;
-        this->_name = name;
-        this->_game = &game;
-        this->_game->playersList.push_back(this->_name);
-        this->_money = 0;
-    }
+    
     Player::~Player()
     {
         int numOfPlayers = 0;
