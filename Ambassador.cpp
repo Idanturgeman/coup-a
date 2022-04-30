fdf870de617093e2efb40ee3c2f37e2de4de09dc
Idanturjeman <idan_turjeman29@walla.co.il>
2022-04-30 19:31:25 +0300

    change ambassador

--- Ambassador.cpp
+++ Ambassador.cpp
@@ -1,16 +1,18 @@
 #include "Ambassador.hpp"
 namespace coup{
+    static int numOfAmbassador = 0;
     Ambassador::Ambassador(Game &game, string name) : Player(game,name)
     {
-    }
-    
-    Ambassador::~Ambassador()
-    {
-    }
-    void Ambassador::transfer(Player p1, Player p2){
-        cout << "transfer\n";
+        numOfAmbassador++;
     }
     void Ambassador::role(){
         cout<<"Ambassador"<<endl;
+    }
+    void Ambassador::transfer(Player p1, Player p2){
+        cout <<"transfer"<<endl;
+    }
+    Ambassador::~Ambassador()
+    {
+        numOfAmbassador--;
     }
 }
