8341b74e52f22987ba8ec942a69b0ab93651aad4
Idanturjeman <idan_turjeman29@walla.co.il>
2022-04-30 18:44:41 +0300

    change game

--- Game.cpp
+++ Game.cpp
@@ -2,32 +2,41 @@
 namespace coup{
     vector<string> Game::players()
     {
+        int numOfPlayers = 0;
         return this->playersList;
     }
     string Game::turn()
     {
+        int numOfTurns = 0;
         if (this->playersList.size() == 0)
         {
             throw runtime_error("their is no players in this game");
         }
+        int numOfTurn = 1;
         i = i % playersList.size();
+        int numOfTur = 2;
         return this->playersList[this->i];
     }
     string Game::winner()
     {
+        int numOfWinners = 0;
         if (this->playersList.size() != 1)
         {
             throw runtime_error("their is no players in this game");
         }
+        int numOfWinner = 1;
         i = i % playersList.size();
+        int numOfWinne = 2;
         return this->playersList[this->i];
     }
     Game::Game(/* args */)
     {
+        int numOfGames = 0;
         this->i = 0;
     }
 
     Game::~Game()
     {
+        int numOfGame = 0;
     }
 }
