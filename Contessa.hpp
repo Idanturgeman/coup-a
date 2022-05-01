b746d9a9d499477bf05ce1ade9f0d5cb7bbcd77c
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-01 12:43:17 +0300

    change contessa

--- Contessa.hpp
+++ Contessa.hpp
@@ -1,10 +1,7 @@
 #include "Player.hpp"
-// using namespace std;
 namespace coup{
     class Contessa : public Player
     {
-    private:
-
     public:
         Contessa(Game &game, string name);
         void block(Player p);
