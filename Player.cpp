6137e3883a54695565679f57b2caedee9567d031
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-01 14:11:51 +0300

    change player

--- Player.cpp
+++ Player.cpp
@@ -12,43 +12,37 @@ namespace coup
     static int numOfCoins = 0;
     Player::Player(Game &game, string name)
     {
-        //setPlayer(Game &game, string name);
-        // game.addPlayer(this);
-        //setName(name);
-        numOfPlayers++;
+        int play = 0;
         this->_name = name;
         this->_game = &game;
-       // int numOfPlaye = 0;
         this->_game->playersList.push_back(this->_name);
-       // int numOfPlay = 0;
         this->_money = 0;
+        numOfPlayers++;
     }
     void Player::income()
     {
-        numOfIncomes++;
+        int inc = 0;
         this->_money++;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
         numOfIncomes++;
-
     }
     void Player::foreign_aid()
     {
-        numOfForeign_aids++;
+        int fore = 0;
         this->_money += 2;
         this->_game->i = (this->_game->i + 1) % this->_game->playersList.size();
         numOfForeign_aids++;
-
     }
 
     void Player::coup(Player p)
     {
-        numOfCoups++;
+        int cou = 0;
         for (unsigned int i = 0; i < this->_game->playersList.size(); i++)
             {
-                numOfCoups++;
+                int j = 0;
                 if (!p._name.compare(this->_game->playersList[i]))
                     {
-                        numOfCoups++;
+                        j++;
                         this->_game->playersList.erase(this->_game->playersList.begin() + i);
                         return;
                     }
